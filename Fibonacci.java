fc3f6af5cbd6cccf96f0bd5837e7a3a8bcf6534e
sonimuthu <soniyam2022@gmail.com>
2026-03-11 19:29:05 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,12 @@
+public class Fibonacci{
+    public static void main(String args[]){
+        int n=5;
+        int a=0,b=1;
+        for(int i=1;i<=n;i++){
+            System.out.print(a+" ");
+            int c = a+b;
+            a=b;
+            b=c;
+        }
+    }
+}
